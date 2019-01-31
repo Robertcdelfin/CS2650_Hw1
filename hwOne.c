6c0451fc455bf001f446c4ed2723414a319f5e4e
Robertcdelfin <42871949+Robertcdelfin@users.noreply.github.com>
2019-01-31 15:39:33 -0800

    Add files via upload

---
+++ hwOne.c
@@ -0,0 +1,16 @@
+/*
+ ============================================================================
+ Name        : hwOne.c
+ Author      : Robert Delfin
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+void main(int argc, char **argv){
+	//Printing function
+	printf("Hello World\n");
+}
